0cea8944e1f7888cd54d24957f96d757b0991013
vipinveluthakkal <vipus2007@gmail.com>
2021-06-15 20:14:43 +0530

    Update test-server.js
    Test File Changes

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [DEMO2] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to [DEMO3] application</h1>"));
         return done();
       });
 
